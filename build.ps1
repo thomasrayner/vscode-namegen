d2693527e6e4dbc7eb2cb9f53bc2281bc4719b17
Thomas Rayner <thmsrynr@outlook.com>
2019-01-27 12:43:32 -0800

    Added package switch to build.ps1

--- build.ps1
+++ build.ps1
@@ -1,2 +1,12 @@
+[cmdletbinding()]
+param (
+    [parameter()]
+    [switch]$Package
+)
+
 npm run compile
-Copy-Item .\src\wordlists .\out\ -Recurse
+Copy-Item .\src\wordlists .\out\ -Recurse -Force
+
+if ($Package) {
+    vsce package
+}
